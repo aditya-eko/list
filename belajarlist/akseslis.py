2e6f2c25c77d6a66a26f3ad02ace8adff35380de
aditya-eko <ekoagusaditya@gmail.com>
2019-08-20 22:14:18 +0700

    menambahkan file akseslist.py

---
+++ belajarlist/akseslis.py
@@ -0,0 +1,22 @@
+from belajarlist import *
+
+
+# mengakses list
+
+
+# mengakses list langsung dengan menggunakan print
+print(cacah[0])
+
+print(cacah[1])
+print(cacah[2])
+
+
+# mengakses dengan memasukan ke dalam sebuah variable
+
+x=cacah[0]
+y=cacah[1]
+z=cacah[2]
+
+print(x)
+print(y)
+print(z)
--- belajarlist/tambahangka.py
+++
@@ -1,6 +0,0 @@
-from belajarlist import *
-
-
-# mengakses list
-
-print(cacah[0])
