38ddf53e930bd3a9ccf753bb83f8b49c2f3d630d
aditya-eko <ekoagusaditya@gmail.com>
2019-08-20 22:42:50 +0700

    menambahkan file angka dan menambahkan list

---
+++ belajarlist/menambahkanlist.py
@@ -0,0 +1,30 @@
+from belajarlist import cacah
+# menambahkan file di belakang
+cacah.append(11)
+cacah.append(12)
+cacah.append(13)
+cacah.append(14)
+
+# menambahkan dengan cara menyisipkan dengan insert
+
+cacah.insert(14,15)
+cacah.insert(15,16)
+cacah.insert(16,17)
+cacah.insert(17,18)
+cacah.insert(18,19)
+cacah.insert(19,20)
+
+
+print(cacah)
+
+
+
+
+
+
+
+
+
+
+
+
