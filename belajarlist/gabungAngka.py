92154b1c003c456984f1454fb19ab00a64454ae2
aditya-eko <ekoagusaditya@gmail.com>
2019-08-21 10:20:27 +0700

    menambahkan file memotong dan gabungangka

--- belajarlist/gabungAngka.py
+++ belajarlist/gabungAngka.py
@@ -3,7 +3,7 @@ from belajarlist import cacah
 cacah2=[11,22,33,44,55,66,77,88,99,100]
 cacah.extend(cacah2)
 
-gabungCacah=(cacah+cacah2)
+gabungCacah=cacah+cacah2
 
 print(gabungCacah)
 print(cacah)
---
+++ memotong.py
@@ -0,0 +1,13 @@
+from belajarlist import cacah
+
+
+memotong1=cacah[0:3]
+memotong2=cacah[4:7]
+
+
+
+print(cacah)
+
+print(memotong1)
+
+print(memotong2)
