4814af37dd31db007bbf29e09be6cf9b529ae292
aditya-eko <ekoagusaditya@gmail.com>
2019-08-22 17:08:32 +0700

    menambahkan file angka.py dan memotong.py

--- memotong.py
+++ memotong.py
@@ -3,11 +3,22 @@ from belajarlist import cacah
 
 memotong1=cacah[0:3]
 memotong2=cacah[4:7]
+memotong3=cacah[6:8]
 
 
-
+memotong4=cacah[1:4]
 print(cacah)
 
 print(memotong1)
 
 print(memotong2)
+print(memotong3)
+print(memotong4)
+
+
+
+memotong5=cacah[1:7]
+print(memotong5)
+
+
+
